469abe08d9a54443be2c05f9fc263f04a26c3199
ilja.tkachuk <tkachuk.labs@gmail.com>
2020-12-15 16:53:01 +0200

    upgrade nixpkgs-master

--- nixpkgs-master.nix
+++ nixpkgs-master.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url="https://github.com/NixOS/nixpkgs/archive/33ea7ef8fdbe272ce3b94c33679c865d6b628021.tar.gz";
-  sha256="01mg7sxdmbawxbwfkjnl3bv7bq13pj54aw5cbr8mb3c6pd9hklwm";
+  url="https://github.com/NixOS/nixpkgs/archive/ba31c04641d63603ee137d455c9031b12ffa5906.tar.gz";
+  sha256="003ix1kdq8iylj5x50nfl5akg8hzkj3shpykpq0g04cgh1q4r48w";
 }
