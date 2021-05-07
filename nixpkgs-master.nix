1654bf03682e450b49121d1451b51794014badfe
tkachuk-labs <tkachuk.labs@gmail.com>
2021-05-07 15:15:34 +0300

    upgrade nixpkgs

--- nixpkgs-master.nix
+++ nixpkgs-master.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
-  url="https://github.com/NixOS/nixpkgs/archive/ba31c04641d63603ee137d455c9031b12ffa5906.tar.gz";
-  sha256="003ix1kdq8iylj5x50nfl5akg8hzkj3shpykpq0g04cgh1q4r48w";
+  url="https://github.com/NixOS/nixpkgs/archive/ae1c8ede09b53007ba9b3c32f926c9c03547ae8b.tar.gz";
+  sha256="1lpphn9dcf8vh8ia38f472i7cqggp3knpfa2jwlc6z5ldbvrw7ki";
 }
