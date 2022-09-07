9973b7589048b2d545e99b1500f00df7ade42f32
tkachuk-labs <tkachuk.labs@gmail.com>
2022-09-08 00:22:42 +0200

    installation logs

--- nix/install.sh
+++ nix/install.sh
@@ -39,3 +39,5 @@ nix-env -i \
   --argstr vimColorScheme $VIM_COLOR_SCHEME \
   --option extra-substituters "https://cache.nixos.org https://hydra.iohk.io" \
   --option trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+
+echo "ultimate-haskell-ide ==> successfully installed with MINI=$MINI"
